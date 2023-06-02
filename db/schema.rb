a5f89973d67f07658163a0ae3f78e661c42acb1c
Reid Miller <reid.s.miller@gmail.com>
2023-06-02 09:43:34 -0600

    setup: Add google id and change password to auth_token

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_02_153228) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_02_153809) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -43,9 +43,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_02_153228) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.string "password_digest"
+    t.string "auth_token"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "google_id"
   end
 
   add_foreign_key "roster_teams", "user_leagues"
