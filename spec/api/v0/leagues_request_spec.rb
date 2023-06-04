a41f9bd5a087f9fcf8c568cd8226edecca7dd3f1
Reid Miller <reid.s.miller@gmail.com>
2023-06-03 19:22:33 -0600

    test/feat: Add create user league when league is created

--- spec/api/v0/leagues_request_spec.rb
+++ spec/api/v0/leagues_request_spec.rb
@@ -3,12 +3,13 @@ require 'rails_helper'
 RSpec.describe 'Leagues Requests' do
   describe 'Create a new league' do
     describe 'Happy Path' do
-      it 'can create a league' do
+      it 'can create a league and associated user league' do
+        user = @user = User.create!(name: 'Bernie Sanders', email: 'feel.the.bernnnn@gmail.com', google_id: '0123456789', auth_token: 'askt.821498435nsdioopa7123h0dfk')
         @new_params = {
           name: 'Test League',
           draft_time: '2000-01-01T12:00:00.000Z',
           draft_date: '2020-08-01',
-          manager_id: 1
+          manager_id: user.id
         }
 
         headers = { 'CONTENT_TYPE' => 'application/json' }
@@ -28,7 +29,7 @@ RSpec.describe 'Leagues Requests' do
 
         expect(data[:data]).to have_key(:id)
         expect(data[:data][:id]).to be_an(String)
-        
+
         expect(data[:data]).to have_key(:attributes)
         expect(data[:data][:attributes]).to be_a(Hash)
 
@@ -37,12 +38,14 @@ RSpec.describe 'Leagues Requests' do
 
         expect(data[:data][:attributes]).to have_key(:draft_time)
         expect(data[:data][:attributes][:draft_time]).to eq(@new_params[:draft_time])
-        
+
         expect(data[:data][:attributes]).to have_key(:draft_date)
         expect(data[:data][:attributes][:draft_date]).to eq(@new_params[:draft_date])
 
         expect(data[:data][:attributes]).to have_key(:manager_id)
         expect(data[:data][:attributes][:manager_id]).to eq(@new_params[:manager_id])
+
+        expect(UserLeague.find_by(user_id: user.id, league_id: data[:data][:id])).to be_a(UserLeague)
       end
     end
 
