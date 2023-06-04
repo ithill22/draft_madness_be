8e160f7a6cb3f10c6f776296e3447db2c5e46aa9
Reid Miller <reid.s.miller@gmail.com>
2023-06-03 20:16:15 -0600

    test: Add test for retrieve all roster teams for a user league

--- spec/api/v0/roster_team_request_spec.rb
+++ spec/api/v0/roster_team_request_spec.rb
@@ -3,8 +3,13 @@ require 'rails_helper'
 RSpec.describe 'RosterTeam Requests' do
   before(:each) do
     @user = @user = User.create!(name: 'Bernie Sanders', email: 'feel.the.bernnnn@gmail.com', google_id: '0123456789', auth_token: 'askt.821498435nsdioopa7123h0dfk')
+    @user2 = User.create!(name: 'Betty White', email: 'always.golden@gmail.com', google_id: '1234567890', auth_token: 'askta.sdasdktsak_jt92sdfafk8476hdk_kasd')
     @league = League.create!(name: 'March Madness', draft_time: '8:00 PM', draft_date: '2021-03-18', manager_id: @user.id)
     @user_league = UserLeague.create!(user_id: @user.id, league_id: @league.id)
+    @user_league2 = UserLeague.create!(user_id: @user2.id, league_id: @league.id)
+    @roster_team1 = RosterTeam.create!(user_league_id: @user_league.id, api_team_id: 'aslkdhkalsdj-aklsjdhlkja-asdlkjhaklsj')
+    @roster_team2 = RosterTeam.create!(user_league_id: @user_league.id, api_team_id: 'mnxcbm-wioueyqoiuwmndsiuqywe-askjdhf')
+    @roster_team3 = RosterTeam.create!(user_league_id: @user_league2.id, api_team_id: 'aslkdhkalsdj-aklsjdhlkja-asdlkjhaklsj')
   end
 
   describe 'can create a new roster_team from params' do
@@ -57,6 +62,37 @@ RSpec.describe 'RosterTeam Requests' do
 
       expect(data).to be_a(Hash)
       expect(data[:errors][:detail]).to eq("Validation failed: Api team can't be blank")
+    end
+  end
+
+  describe 'can retrieve all roster_teams for a user_league' do
+    it 'happy path' do
+      get "/api/v0/user_leagues/#{@user_league[:id]}/roster_teams"
+
+      expect(response).to be_successful
+      expect(response).to have_http_status(200)
+
+      data = JSON.parse(response.body, symbolize_names: true)
+
+      expect(data).to be_a(Hash)
+      expect(data[:data]).to be_an(Array)
+      expect(data[:data].count).to eq(2)
+      roster_team = data[:data].first
+
+      expect(roster_team).to have_key(:id)
+      expect(roster_team[:id]).to be_a(String)
+
+      expect(roster_team).to have_key(:type)
+      expect(roster_team[:type]).to eq('roster_team')
+
+      expect(roster_team).to have_key(:attributes)
+      expect(roster_team[:attributes]).to be_a(Hash)
+
+      expect(roster_team[:attributes]).to have_key(:user_league_id)
+      expect(roster_team[:attributes][:user_league_id]).to be_a(Integer)
+
+      expect(roster_team[:attributes]).to have_key(:api_team_id)
+      expect(roster_team[:attributes][:api_team_id]).to be_a(String)
     end
   end
 end
