5d6752a477a6304f217b5c0e6f2c0a91dff32504
Katie Lonsdale <katie952@gmail.com>
2023-06-07 19:36:31 -0600

    test: add test for id

--- spec/poros/article_spec.rb
+++ spec/poros/article_spec.rb
@@ -3,11 +3,12 @@ require 'rails_helper'
 RSpec.describe Article do
   before(:each) do
     article_data
-    @article = Article.new(@data)
+    @article = Article.new(1, @data)
   end
   describe 'initialize' do
     it 'exists and has attributes' do
       expect(@article).to be_a(Article)
+      expect(@article.id).to eq(1)
       expect(@article.headline).to eq('Times sports writer Pete Thamel breaks down the NCAA tournament brackets and highlights the teams to watch. (Producer: Kassie Bracken)')
       expect(@article.url).to eq("https://www.nytimes.com/video/sports/1194817114941/ncaa-tournament-preview.html")
       expect(@article.date).to eq('March 13, 2006')
